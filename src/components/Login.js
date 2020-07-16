8c88fb546ec0e9da7cc3f65014e588b99bf3945d
Fabiola Deiro <fabioladeiro7@gmail.com>
2020-07-16 15:52:18 -0300

    Primeira tela

--- src/components/Login.js
+++ src/components/Login.js
@@ -5,32 +5,61 @@ import PropTypes from 'prop-types';
 import { fetchToken } from '../actions/index';
 
 class Login extends Component {
-  render() {
+  constructor(props) {
+    super(props);
+    this.state = { gamerName: '', gamerEmail: '' };
+    this.handle = this.handle.bind(this);
+    this.buttonFunction = this.buttonFunction.bind(this);
+  }
+
+  handle(event) {
+    const { name, value } = event.target;
+    this.setState({ [name]: value });
+  }
+
+  buttonFunction(state) {
     const { getTokenGamer } = this.props;
+    return state.gamerEmail !== '' && state.gamerName !== '' ? (
+      <Link to="/game-screen">
+        <button type="submit" data-testid="btn-play" onClick={() => getTokenGamer()}>
+          Jogar
+        </button>
+      </Link>
+    ) : (
+      <button type="button" disabled="disabled" data-testid="btn-play">
+        Jogar
+      </button>
+    );
+  }
+
+  render() {
+    const { handle } = this;
     return (
       <div>
-        <form>
-          <label htmlFor="gamer-name">Name
-            <input
-              type="text" required="required" name="gamer-name" data-testid="input-player-name"
-            />
-          </label>
-          <label htmlFor="gamer-email">Email
-            <input
-              type="email" required="required" name="gamer-email" data-testid="input-gravatar-email"
-            />
-          </label>
-          <Link to="/game-screen">
-            <button type="submit" data-testid="btn-play" onClick={() => getTokenGamer()}>
-              Jogar
-            </button>
-          </Link>
-          <Link to="/settings">
-            <button data-testid="btn-settings">
-              Settings
-            </button>
-          </Link>
-        </form>
+        <label htmlFor="gamerName">
+          Name
+          <input
+            type="text"
+            required="required"
+            name="gamerName"
+            data-testid="input-player-name"
+            onChange={(e) => handle(e)}
+          />
+        </label>
+        <label htmlFor="gamerEmail">
+          Email
+          <input
+            type="email"
+            required="required"
+            name="gamerEmail"
+            data-testid="input-gravatar-email"
+            onChange={(e) => handle(e)}
+          />
+        </label>
+        {this.buttonFunction(this.state)}
+        <Link to="/settings">
+          <button data-testid="btn-settings">Settings</button>
+        </Link>
       </div>
     );
   }
