9948f86e779a9997edd75976305ae2b5440cee04
Josias Viveiro <josiasviveiro@gmail.com>
2020-07-16 16:17:10 -0300

    Adicionado o player ao reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
 import tokenApi from './tokenApi';
+import player from './player';
 
 const rootReducer = combineReducers({
   tokenApi,
+  player,
 });
 
 export default rootReducer;
