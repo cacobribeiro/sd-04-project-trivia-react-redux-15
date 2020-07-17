86a609654363aa200f902a480662509516172635
cacobribeiro <cacobribeiro@gmail.com>
2020-07-17 16:28:55 -0300

    Refatorando - 20 linhas

--- src/components/Question.js
+++ src/components/Question.js
@@ -6,29 +6,12 @@ function Questions({ data, QuestionsLoading }) {
   const index = 0;
   if (QuestionsLoading) return <p>L O A D I N G . . . </p>;
   const questions = [...data[index].incorrect_answers, data[index].correct_answer].sort();
-  if (data[index].type === 'boolean') {
-    return (
-      <div>
-        <div>
-          <h2 data-testid="question-text">{data[index].question}</h2>
-          <small data-testid="question-category">{data[index].category}</small>
-        </div>
-        {questions.map((e, indexWrong) => {
-          if (data[index].correct_answer === e) {
-            return <button data-testid="correct-answer">{e}</button>;
-          }
-          return <button data-testid={`wrong-answer-${indexWrong}`}>{e}</button>;
-        })}
-      </div>
-    );
-  }
   return (
     <div>
       <div>
         <h2 data-testid="question-text">{data[index].question}</h2>
         <small data-testid="question-category">{data[index].category}</small>
       </div>
-      {console.log(questions)}
       {questions.map((e, indexWrong) => {
         if (data[index].correct_answer === e) {
           return <button data-testid="correct-answer">{e}</button>;
