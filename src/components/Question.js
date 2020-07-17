524429bc2e46b785feb34fd33d61d7420b3ddca2
cacobribeiro <cacobribeiro@gmail.com>
2020-07-17 16:26:18 -0300

    Multiplas escolhas e true ou false

--- src/components/Question.js
+++ src/components/Question.js
@@ -3,28 +3,30 @@ import React from 'react';
 import { connect } from 'react-redux';
 
 function Questions({ data, QuestionsLoading }) {
-  const index = 1;
+  const index = 0;
   if (QuestionsLoading) return <p>L O A D I N G . . . </p>;
+  const questions = [...data[index].incorrect_answers, data[index].correct_answer].sort();
   if (data[index].type === 'boolean') {
     return (
       <div>
         <div>
           <h2 data-testid="question-text">{data[index].question}</h2>
           <small data-testid="question-category">{data[index].category}</small>
-          <small>{data[index].difficulty}</small>
         </div>
-        <button>TRUE</button>
-        <button>FALSE</button>
+        {questions.map((e, indexWrong) => {
+          if (data[index].correct_answer === e) {
+            return <button data-testid="correct-answer">{e}</button>;
+          }
+          return <button data-testid={`wrong-answer-${indexWrong}`}>{e}</button>;
+        })}
       </div>
     );
   }
-  const questions = [...data[index].incorrect_answers, data[index].correct_answer].sort();
   return (
     <div>
       <div>
-        <h2>{data[index].question}</h2>
-        <small>{data[index].category}</small>
-        <small>{data[index].difficulty}</small>
+        <h2 data-testid="question-text">{data[index].question}</h2>
+        <small data-testid="question-category">{data[index].category}</small>
       </div>
       {console.log(questions)}
       {questions.map((e, indexWrong) => {
